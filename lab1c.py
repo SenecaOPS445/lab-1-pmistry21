8c6674b6f414cd5c018481eccb1053118d045be5
Prasad Mistry <pmistry21@myseneca.ca>
2024-05-16 18:13:56 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#Description: This program will use strings name and age, and output "Isaac is 72 years old!".
+name = 'Isaac'
+age = '72'
+print(name + ' is ' + str(age) + ' years old!')
+
