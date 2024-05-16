20c91e935805f54f337bebabef707fd3912773e1
Prasad Mistry <pmistry21@myseneca.ca>
2024-05-16 16:36:05 -0400

    Added lab1b

--- lab1b.py
+++ lab1b.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#Description: This program will use a string to output "How old are you Isaac?".
+#Description: This program will use a string and output "How old are you Isaac?".
 name = 'Isaac'
 print('How old are you ' + name + '?')
 
