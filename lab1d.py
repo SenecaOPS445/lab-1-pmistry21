566ce8a08048b5545fcd0407879e353677d9ebe7
Prasad Mistry <pmistry21@myseneca.ca>
2024-05-16 18:44:31 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#Description: This program will use the objects x, y, and z, and output the sum of "10 + 2 * 5".
+x = 10
+y = 2
+z = 5
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(x + y * z))
